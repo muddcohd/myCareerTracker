1a8550e09ac622f1252f93370d105049e7612983
Scott <muddcohd@gmail.com>
2023-03-04 23:45:52 -0500

    finished controllers

---
+++ backend/controllers/jobController.js
@@ -0,0 +1,79 @@
+const Job = require('../models/jobModel');
+const asyncHandler = require('express-async-handler');
+const mongoose = require('mongoose');
+
+//get all jobs
+const getAllJobs = asyncHandler(async (req, res) => {
+  const jobs = await Job.find({}).sort({ createdAt: -1 });
+
+  if (!jobs) {
+    res.status(404).json({ error: 'There are no jobs found' });
+  }
+
+  res.status(200).json(jobs);
+});
+
+//get single job
+const getJob = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such job' });
+  }
+
+  const job = await Job.findById(id);
+
+  if (!job) {
+    return res.status(404).json({ error: 'No such job' });
+  }
+
+  return res.status(200).json(job);
+});
+
+//create new job
+const createJob = asyncHandler(async (req, res) => {
+  const { title, company, location, salary } = req.body;
+
+  const job = await Job.create({ title, company, salary, location });
+
+  res.status(200).json(job);
+});
+
+//delete job
+const deleteJob = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such job' });
+  }
+
+  const job = await Job.findOneAndDelete({ _id: id });
+
+  if (!job) {
+    return res.status(404).json({ error: 'No such job' });
+  }
+  res.status(200).json(job);
+});
+//update a job
+const updateJob = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such job' });
+  }
+
+  const job = await Job.findByIdAndUpdate({ _id: id }, { ...req.body });
+
+  if (!job) {
+    return res.send(404).json({ error: 'No such job' });
+  }
+  res.status(200).json(job);
+});
+
+module.exports = {
+  createJob,
+  getAllJobs,
+  getJob,
+  deleteJob,
+  updateJob,
+};
--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -1,40 +1,15 @@
 const express = require('express');
-const asyncHandler = require('express-async-handler');
+const {
+  createJob,
+  getAllJobs,
+  getJob,
+  deleteJob,
+  updateJob,
+} = require('../controllers/jobController');
 
 const router = express.Router();
-const Job = require('../models/jobModel');
 
-router.get('/', (req, res) => {
-  res.json({ msg: 'GET all jobs' });
-});
+router.route('/').post(createJob).get(getAllJobs);
+router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob);
 
-router.get('/:id', (req, res) => {
-  res.json({ msg: 'GET a single job' });
-});
-
-//POST a new job
-router.post(
-  '/',
-  asyncHandler(async (req, res) => {
-    const { title, company, salary, location } = req.body;
-    if (!title || !company || !location) {
-      return res
-        .status(400)
-        .json({ msg: 'Title, company and location are required' });
-    }
-
-    const job = await Job.create({ title, company, salary, location });
-
-    res.status(200).json(job);
-  })
-);
-//UPDATE router.post('/:id', (req,res)=>{
-router.patch('/:id', (req, res) => {
-  res.json({ msg: 'UPDATE a job' });
-});
-
-//DELETE a  job
-router.delete('/:id', (req, res) => {
-  res.json({ msg: 'DELETE job' });
-});
 module.exports = router;
