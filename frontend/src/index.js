92bda603993d71d26a8928b3a6175c645cd9af62
Scott <muddcohd@gmail.com>
2023-03-10 17:31:02 -0500

    sarted autho, created auth context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { JobsContextProvider } from './context/JobContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <JobsContextProvider>
-      <App />
-    </JobsContextProvider>
+    <AuthContextProvider>
+      <JobsContextProvider>
+        <App />
+      </JobsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
